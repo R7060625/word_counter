a395f75bba6ae65b8b39603ddc96c02b6c722c72
R7060625 <liberaleraphael@gmail.com>
2025-12-25 18:52:35 +0100

    Add files via upload

---
+++ wc.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <string.h>
+#define nonwords " ,.;:!?\n\t"
+
+/*PREMIER ALGORITHME : 
+    rangement itératif des mots; Dès qu'un nouveau mot est rencontré,
+    on le rajoute à notre liste dynamique avec son nombre d'itérations. 
+    On trie ensuite la liste de manière décroissante afin que les mots soient
+    rangés dans l'ordre décroissant. 
+    Complexité : O(n²) pour le rangement des mots, 
+                 O(n log(n)) pour le tri de la liste
+*/
+
+typedef struct {
+    int* cumul_alloc; // liste dynamique de chaînes
+    int*  cumul_desalloc; // champ obligatoire : cumul de l’espace mémoire désalloué
+    int* max_alloc;
+} InfoMem;
+
+typedef struct {
+    int iteration;
+    char* mot[];
+} Mot;
+
+
+
+int compteMots_algo1(FILE *fichier, Mot* liste_iter) {
+
+    int compteur = 1;
+
+    int c;
+    while ((c = fgetc(fichier)) != EOF) {
+        if (strchr(nonwords, c)) {
+            
+            c = fgetc(fichier);
+
+            // tant qu'il y a un separateur, on prend le nouveau caractere
+            while (strchr(nonwords, c)) {
+                c = fgetc(fichier);
+            };
+
+            compteur++;
+        };
+    }
+    return compteur;
+}
+
+
+
+int main(int argc, char* argv[]) {
+
+    Mot* liste_iter = (Mot *) malloc(1000 * sizeof(Mot)); // On commence par ajouter 1000 emplacements de mots dans la liste 
+
+    FILE *file = fopen(argv[1], "r");
+        printf("Il y a %d mots dans le fichier %s\n", compteMots_algo1(file, liste_iter), argv[1]);
+
+        /*char string[500];
+        assigneLigne(file, string);
+
+        printf("La ligne est %s", string );*/
+        fclose(file);
+
+
+    return 0;
+}
